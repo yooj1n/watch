b83fd9d8aef5ee5dd7bf7bf92fd6082b09893ace
YOOJIN <ujin07150@gmail.com>
2021-12-19 16:25:03 +0900

    개봉예정, 인기 챕터

--- Movies.js
+++ Movies.js
@@ -6,14 +6,18 @@ import StarRating from 'react-native-star-rating';
 const API_KEY = "0a5c0d8acd8423f2bb24153b5937e1c7"
 
 const Container = styled.ScrollView`
+  margin-top: 63px;
   margin-left: 16px;
 `;
 
-const NowPText = styled.Text`
+const NowPContainer = styled.View`
+margin-bottom: 40px;
+`
+
+const List = styled.Text`
   font-size: 20px;
   font-weight: 700;
   line-height: 29px;
-  margin-top: 43px;
 `;
 
 const NowPScroll = styled.ScrollView`
@@ -27,14 +31,50 @@ const Movie = styled.View`
 
 const Title = styled.Text`
 font-size: 12px;
+font-weight: 500;
 margin-top: 10px;
 margin-bottom: 5px;
 `
-const Vote = styled.Text``
+
+const VWrap = styled.View`
+margin-bottom: 40px;
+`
+
+const ComingWrap = styled.View`
+display: flex;
+flex-direction: column;
+width: 100%;
+margin-left: 16px;
+`
+
+const Coming = styled.View`
+  flex-direction: row;
+  margin-top: 16px;
+`;
+
+const ComingDesc = styled.View`
+  flex-direction: column;
+  /* margin-left: 16px; */
+  font-size: 10px;
+`
+
+const Vote = styled.View`
+
+
+`
+
+const Release = styled.Text`
+position: absolute;
+bottom: 5px;
+right: 20px;
+color: #9A9A9A;
+`
 
 
 const Movies = () => {
-  const [nowPlaying, setNowPlaying] = useState([])
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [popular, setPopular] = useState([]);
   const getNowPlaying = async () => {
     const { results } = await (
       await fetch(
@@ -43,35 +83,108 @@ const Movies = () => {
     ).json();
     setNowPlaying(results);
   };
+  const getUpcoming = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+      )
+    ).json();
+    setUpcoming(results);
+  };
+  const getPopular = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+      )
+    ).json();
+    setPopular(results);
+  };
   useEffect(() => {
     getNowPlaying();
+    getUpcoming();
+    getPopular();
   }, []);
   return (
     <Container>
-      <NowPText> 현재 상영중</NowPText>
-      <NowPScroll 
-        horizontal
-        showsHorizontalScrollIndicator={false}
-      >
-        {nowPlaying.map(
-          (movie) => (
-          <Movie key={movie.id}>
-            <Poster path={movie.poster_path} />
-            <Title>
-              {movie.original_title.slice(0, 13)}
-              {movie.original_title.length > 13 ? "..." : null}
-            </Title>
+      <NowPContainer>
+        <List>현재 상영중</List>
+        <NowPScroll 
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        >
+          {nowPlaying.map(
+            (movie) => (
+            <Movie key={movie.id}>
+              <Poster path={movie.poster_path} />
+              <Title>
+                {movie.original_title.slice(0, 13)}
+                {movie.original_title.length > 13 ? "..." : null}
+              </Title>
+              <StarRating
+                disabled={true}
+                starSize={9}
+                maxStars={5}
+                rating={movie.vote_average / 2}
+                fullStarColor={'#F1C644'}
+                emptyStarColor={'#C4C4C4'}
+                starStyle={{marginRight:3}}
+              />
+            </Movie>
+          ))}
+        </NowPScroll>
+      </NowPContainer>
+      <VWrap>
+      <List>개봉 예정</List>
+      {upcoming.slice(0,3).map((movie) => (
+      <Coming key={movie.id}>
+        <Poster path={movie.poster_path}/>
+        <ComingWrap>
+          <ComingDesc>
+            <Title>{movie.original_title}</Title>
+          </ComingDesc>
+          <Vote>
             <StarRating
               disabled={true}
               starSize={9}
               maxStars={5}
               rating={movie.vote_average / 2}
               fullStarColor={'#F1C644'}
-              emptyStarColor={'#F1C644'}
+              emptyStarColor={'#C4C4C4'}
+              containerStyle={{width:"15%"}}
             />
-          </Movie>
-        ))}
-      </NowPScroll>
+           </Vote>
+           </ComingWrap> 
+          <Release>
+            {new Date(movie.release_date).toISOString().split('T')[0]}
+          </Release>
+      </Coming>))}
+      </VWrap>
+      <VWrap>
+      <List>인기</List>
+      {popular.slice(0,3).map((movie) => (
+      <Coming key={movie.id}>
+        <Poster path={movie.poster_path}/>
+        <ComingWrap>
+          <ComingDesc>
+            <Title>{movie.original_title}</Title>
+          </ComingDesc>
+          <Vote>
+            <StarRating
+              disabled={true}
+              starSize={9}
+              maxStars={5}
+              rating={movie.vote_average / 2}
+              fullStarColor={'#F1C644'}
+              emptyStarColor={'#C4C4C4'}
+              containerStyle={{width:"15%"}}
+            />
+           </Vote>
+           </ComingWrap> 
+          <Release>
+            {new Date(movie.release_date).toISOString().split('T')[0]}
+          </Release>
+      </Coming>))}
+      </VWrap>
     </Container>
   )
 };
--- Poster.js
+++ Poster.js
@@ -4,11 +4,12 @@ const Image = styled.Image`
   width: 104px;
   height: 160px;
   border-radius: 8px;
+  background-color: black;
 `;
 
-export const makeImgPath = (img) =>
-  `https://image.tmdb.org/t/p/w500${img}`;
+export const makeImgPath = (path) =>
+  `https://image.tmdb.org/t/p/w500/${path}`;
 
 export const Poster = ({ path }) => (
-    <Image source={{ uri: makeImgPath(path) }} />
+    <Image source={{ uri: makeImgPath(path)}} />
   );
